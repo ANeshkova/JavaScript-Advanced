792780b3f88444cfd48cb8dafb5197e3ba057654
ANeshkova <anitowurth@abv.bg>
2020-12-28 19:48:21 +0200

    Added solution

---
+++ 01.1 Syntax, Functions and Statements - Lab/03. SumOfNumbersN…M.js
@@ -0,0 +1,14 @@
+function sumOfNumbersNM(num1, num2) {
+
+    num1 = Number(num1);
+    num2 = Number(num2);
+    let sum = 0;
+
+    for (let i = num1; i <= num2; i++) {
+        sum += i;
+    }
+
+    console.log(sum);
+}
+
+sumOfNumbersNM('1', '5');
