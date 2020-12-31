41abc1addbdb97e9ad5cfda7d235bab9f6a59c55
ANeshkova <anitowurth@abv.bg>
2020-12-31 18:35:19 +0200

    Added solution

---
+++ 01.1 Syntax, Functions and Statements - Lab/04. LargestNumber.js
@@ -0,0 +1,21 @@
+function largestNumber(num1, num2, num3) {
+
+    let max = Number.MIN_SAFE_INTEGER;
+
+    if (num1 > max) {
+        max = num1;
+    }
+
+    if (num2 > max) {
+        max = num2;
+    }
+
+    if (num3 > max) {
+        max = num3;
+    }
+
+    console.log(`The largest number is ${max}.`);
+}
+
+largestNumber(5, -3, 16);
+//largestNumber(-3, -5, -22.5);
