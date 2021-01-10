d85f8e7e921c7f0aa2386ac25751c5038f1e1eb5
ANeshkova <anitowurth@abv.bg>
2021-01-10 10:23:33 +0000

    Added solution

---
+++ 02.2 Arrays - Exercise/01. PrintArrayWithGivenDelimiter.js
@@ -0,0 +1,15 @@
+function printArrayWithGivenDelimiter(input) {
+
+    let delimiter = input.pop();
+
+    console.log(input.join(delimiter));
+}
+
+printArrayWithGivenDelimiter([
+    'One',
+    'Two',
+    'Three',
+    'Four',
+    'Five',
+    '-'
+]);
