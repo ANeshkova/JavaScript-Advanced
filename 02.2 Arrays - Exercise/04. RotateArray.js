612ec8e03d99f2f69e1312de3bd8e7b1a08985ae
ANeshkova <anitowurth@abv.bg>
2021-01-11 22:18:31 +0000

    Added solution

---
+++ 02.2 Arrays - Exercise/04. RotateArray.js
@@ -0,0 +1,18 @@
+function rotateArray(input) {
+
+    let rotation = Number(input.pop()) % input.length;
+
+    for (let i = 0; i < rotation; i++) {
+        input.unshift(input.pop());
+    }
+
+    console.log(input.join(' '));
+}
+
+rotateArray([
+    '1',
+    '2',
+    '3',
+    '4',
+    '2'
+]);
